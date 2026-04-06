233d5a2a7d5d064a4cd40d1af5fa67a3bec11e74
ahmedabdo404 <ahmed.elzahed404@gmail.com>
2026-04-06 22:55:36 +0200

    add and fix tests in CI

--- src/app/Services/get-data.service.spec.ts
+++ src/app/Services/get-data.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { GetDataService } from './get-data.service';
 
@@ -6,7 +7,9 @@ describe('GetDataService', () => {
   let service: GetDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(GetDataService);
   });
 
